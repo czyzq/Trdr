4b60b5e5b1c7b5096e454030e0a23393fed74aea
pinchr <pinchr@MacBook-Air-Agnieszka.local>
2026-02-16 00:24:33 +0100

    fix port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,12 +13,12 @@ export default defineConfig({
     ],
     proxy: {
       '/cfd/api': {
-        target: 'http://localhost:9999',
+        target: 'http://localhost:8801',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/cfd\/api/, '/api')
       },
       '/api': {
-        target: 'http://localhost:9999',
+        target: 'http://localhost:8001',
         changeOrigin: true
       }
     }
